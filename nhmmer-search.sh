6a13b9da1d4eb4b046db6192432043ec9bede556
lilychughes <lilychughes@gmail.com>
2020-02-11 13:48:57 -0600

    Update nhmmer-search.sh
    fixed hmm directory path mistake

--- nhmmer-search.sh
+++ nhmmer-search.sh
@@ -11,7 +11,7 @@ if [  ! -e $directory.nhmmer.txt  ];
 then
 echo nhmmer search started $directory > $directory.nhmmer.txt;
 cd $directory;
-cp ../hmm/*.hmm .
+cp ../../hmm/*.hmm .
 for hmm in *hmm;
 do
 nhmmer --tblout $hmm-hits.txt --noali -T 100 --cpu 2 --dna $hmm $directory.fasta;
